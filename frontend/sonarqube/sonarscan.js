e0907f2837889a13287c0bdee742f365b2fc6efa
Atharv Vani <atharvavani110@gmail.com>
2024-01-25 01:58:00 +0530

    SonarQube: Frontend Test Pass

--- frontend/sonarqube/sonarscan.js
+++ frontend/sonarqube/sonarscan.js
@@ -5,12 +5,12 @@ import process from 'process';
 scanner(
     {
         serverUrl: 'http://localhost:9000',
-        login: "sqp_811f39ab83cf0544d977d2773b78c38b76ae51ef",
+        login: "sqp_ce8ae07f43f1eba00316deca18ddb47ad59e658b",
         options: {
             'sonar.projectName': 'ezmark-frontend',
             'sonar.projectDescription': 'ezmark is an managemenet system that manages attendance and the students',
             'sonar.projectKey': 'sonarqube-ezmark',
-            'sonar.projectVersion': '0.0.1',
+            'sonar.projectVersion': '0.0.2',
             'sonar.sources': 'src',
             'sonar.exclusions': '',
             'sonar.sourceEncoding': 'UTF-8',
